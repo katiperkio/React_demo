9f1bbe2af53f152821c568b8832dc4fecf6f86f9
katiperkio <perkiokati@outlook.com>
2024-03-25 14:44:23 +0200

    add, decrease and reset handlers

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,35 @@ import { useState } from "react";
 import Box from "./Box";
 
 function App() {
-  const [counter, setCounter] = useState(100);
+  const [counter, setCounter] = useState(0);
   const [persons, setPersons] = useState([
-    { name: "Kati", title: "CEO", location: "Vantaa" },
-    { name: "Joni", title: "CTO", location: "Helsinki" },
-    { name: "Heli", title: "designer", location: "Tampere" },
+    { id: 1, name: "Kati", title: "CEO", location: "Vantaa" },
+    { id: 2, name: "Joni", title: "CTO", location: "Helsinki" },
+    { id: 3, name: "Heli", title: "designer", location: "Tampere" },
   ]);
+
+  const addHandler = () => {
+    console.log("Foxes forever");
+    setCounter(counter + 1);
+  };
+
+  const removeHandler = () => {
+    setCounter(counter - 1);
+  };
+
+  const resetHandler = () => {
+    setCounter(0);
+  };
 
   return (
     <>
       <p>Counter: {counter}</p>
+      <button onClick={addHandler}>+</button>
+      <button onClick={removeHandler}>-</button>
+      <button onClick={resetHandler}>reset</button>
       {persons.map((person) => (
         <Box
+          key={person.id} //has to be unique
           name={person.name}
           title={person.title}
           location={person.location}
